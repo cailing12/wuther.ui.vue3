9cebc1c29b9dd38972009d90d04b84f3f385d3b8
cailing12 <caiyi168@qq.com>
2021-05-23 12:51:34 +0800

    blog view

--- src/views/blog/index.vue
+++ src/views/blog/index.vue
@@ -1,38 +1,63 @@
 <template>
-  <el-form ref="form" :model="form" label-width="5em">
-    <el-form-item label="标题">
-      <el-input v-model="form.title"></el-input>
+  <el-form ref="ruleForm" :rules="rules" :model="ruleForm" label-width="10em">
+    <el-form-item label="标题" prop="title">
+      <el-input v-model="ruleForm.title"></el-input>
     </el-form-item>
-    <el-form-item label="类别">
-      <el-radio-group v-model="form.menuId">
+    <el-form-item label="类别" prop="menuId">
+      <el-radio-group v-model="ruleForm.menuId">
         <el-radio
           v-for="menu in menuList"
           :key="menu.item.id"
           :label="menu.item.id"
           :value="menu.item.id"
-        >{{menu.item.name}}</el-radio>
+          >{{ menu.item.name }}</el-radio
+        >
+      </el-radio-group>
+    </el-form-item>
+    <el-form-item label="是否公开" prop="isPublic">
+      <el-radio-group v-model="ruleForm.isPublic">
+        <el-radio key="0" label="0" value="0">是</el-radio>
+        <el-radio key="1" label="1" value="0">否</el-radio>
       </el-radio-group>
     </el-form-item>
   </el-form>
   <div id="editContent" ref="editor"></div>
   <!-- <button @click="syncHTML">同步内容</button> -->
-  <el-button type="primary" id="blogSubmit" @click="submit">提交</el-button>
-  <div :innerHTML="content.html"></div>
+  <el-button type="primary" id="blogSubmit" @click="submit('ruleForm')"
+    >提交</el-button
+  >
 </template>
 
 <script>
 import { onMounted, onBeforeUnmount, ref, reactive } from "vue";
-import { addBlog } from '@/api/blog'
+import { addBlog } from "@/api/blog";
 import WangEditor from "wangeditor";
 import { mapGetters } from "vuex";
+import { ElMessage } from "element-plus";
 export default {
   name: "app",
   data() {
     return {
       //menuList: this.$store.state.menu.verticalNavList,
-      form: {
+      ruleForm: {
         title: "",
         menuId: "",
+        isPublic: "0",
+      },
+      rules: {
+        title: [
+          { required: true, message: "请输入标题", trigger: "blur" },
+          {
+            min: 3,
+            max: 30,
+            message: "长度在 3 到 30 个字符",
+            trigger: "blur",
+          },
+        ],
+        menuId: [{ required: true, message: "请选择类别", trigger: "change" }],
+        isPublic: [
+          { required: true, message: "请选择是否公开", trigger: "change" },
+        ],
       },
     };
   },
@@ -50,12 +75,55 @@ export default {
     let instance;
     onMounted(() => {
       instance = new WangEditor(editor.value);
-      Object.assign(instance.config, {
-        onchange() {
-          console.log("change");
+      instance.config.uploadImgServer = "https://localhost:6088/api/UploadFile";
+      instance.config.uploadImgParams = {
+        token: "xxxxx",
+        x: 100,
+      };
+      instance.config.uploadImgHooks = {
+        // 上传图片之前
+        before: function (xhr) {
+          console.log(xhr);
+
+          // 可阻止图片上传
+          // return {
+          //   prevent: true,
+          //   msg: "需要提示给用户的错误信息",
+          // };
         },
-        height:500
-      });
+        // 图片上传并返回了结果，图片插入已成功
+        success: function (xhr) {
+          console.log("success", xhr);
+        },
+        // 图片上传并返回了结果，但图片插入时出错了
+        fail: function (xhr, editor, resData) {
+          console.log("fail", resData);
+        },
+        // 上传图片出错，一般为 http 请求的错误
+        error: function (xhr, editor, resData) {
+          console.log("error", xhr, resData);
+        },
+        // 上传图片超时
+        timeout: function (xhr) {
+          console.log("timeout", xhr);
+        },
+        // 图片上传并返回了结果，想要自己把图片插入到编辑器中
+        // 例如服务器端返回的不是 { errno: 0, data: [...] } 这种格式，可使用 customInsert
+        // customInsert: function (insertImgFn, result) {
+        //   // result 即服务端返回的接口
+        //   console.log("customInsert", result);
+
+        //   // insertImgFn 可把图片插入到编辑器，传入图片 src ，执行函数即可
+        //   insertImgFn(result.data[0]);
+        // },
+      };
+
+      // Object.assign(instance.config, {
+      //   onchange() {
+      //     console.log("change");
+      //   },
+      //   height:500
+      // });
       instance.create();
     });
     onBeforeUnmount(() => {
@@ -72,21 +140,32 @@ export default {
     };
   },
   methods: {
-    submit() {
-      var entity = {
-        userId:'1',
-        title:this.form.title,
-        menuId:this.form.menuId,
-        content:this.content.html
-      }
-      const { data } =  addBlog(entity)
-      console.log(data);
+    submit(formName) {
+      this.syncHTML();
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          var entity = {
+            userId: "1",
+            title: this.ruleForm.title,
+            menuId: this.ruleForm.menuId,
+            content: this.content.html,
+          };
+          const { data } = addBlog(entity);
+          console.log("保存消息");
+          console.log(data);
+        } else {
+          ElMessage.error("博客保存出错");
+        }
+      });
     },
+  },
+  resetForm(formName) {
+    this.$refs[formName].resetFields();
   },
 };
 </script>
 <style lang="less" scoped>
-#blogSubmit{
+#blogSubmit {
   margin: 1em;
   float: right;
 }
